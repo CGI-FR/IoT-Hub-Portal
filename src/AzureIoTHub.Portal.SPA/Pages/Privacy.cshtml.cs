f24f9e299a20c282e71f7e13df4340de177a1dd9
Beaugrand, Kevin <kevin.beaugrand@cgi.com>
2021-03-13 18:32:37 +0100

    Initial Commit

---
+++ src/AzureIoTHub.Portal.SPA/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) kbeaugrand@gmail.com. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.SPA.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Devices/GetDevices.cs
@@ -0,0 +1,39 @@
+// Copyright (c) kbeaugrand@gmail.com. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Devices
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+
+    using Newtonsoft.Json;
+
+    public static class GetDevices
+    {
+        [FunctionName("GetDevices")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
