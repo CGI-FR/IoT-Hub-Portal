aaff5906f328a5190b8aa271d9044ef9e73eed6f
ben salim <salim.benahben@cgi.com>
2021-08-20 11:38:13 +0200

    affichage des gateways sous forme de liste

---
+++ src/AzureIoTHub.Portal/Shared/Models/Gateway.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Gateway
+    {
+        public string DeviceID { get; set; }
+
+        public string TypeOfGateway { get; set; }
+
+        public string Status { get; set; }
+
+        public int NbDevice { get; set; }
+
+        public int NbModule { get; set; }
+
+        public string Environement { get; set; }
+    }
+}
