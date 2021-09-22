79ffc276d6c98a5c4e2cc417780e85b4cad8a938
ben salim <salim.benahben@cgi.com>
2021-09-22 10:10:29 +0200

    affichage du endpoint,Scope,SymmetricKey d'un device

---
+++ src/AzureIoTHub.Portal/Shared/Models/ConfigItem.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ConfigItem
+    {
+        public string Name { get; set; }
+
+        public DateTime DateCreation { get; set; }
+
+        public string Status { get; set; }
+
+        public ConfigItem()
+        {
+            this.DateCreation = new DateTime(1999, 1, 1);
+        }
+    }
+}
