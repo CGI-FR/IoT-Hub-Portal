89fa0ec689667eaac68d060b47a7aa11a4acdb3f
Audrey Serra <audrey.serra@cgi.com>
2021-09-22 14:51:22 +0200

    DeviceListItem.cs deplacé dans Models Ajout Liste Configurations

---
+++ src/AzureIoTHub.Portal/Shared/Models/ConfigListItem.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models
+{
+    using System;
+
+    public class ConfigListItem
+    {
+        public string ConfigurationID { get; set; }
+
+        public string Conditions { get; set; }
+
+        public long MetricsTargeted { get; set; }
+
+        public long MetricsApplied { get; set; }
+
+        public long MetricsSuccess { get; set; }
+
+        public long MetricsFailure { get; set; }
+
+        public int Priority { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
