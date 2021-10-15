be62818d0c5fb1fb7e6f7f02f0b8ef1a3d4fa29e
ben salim <salim.benahben@cgi.com>
2021-10-15 17:05:03 +0200

    formulaire ajout sensor model

---
+++ src/AzureIoTHub.Portal/Shared/Models/SensorCommand.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SensorCommand
+    {
+        public string Name { get; set; }
+
+        public string Trame { get; set; }
+
+        public int Port { get; set; }
+    }
+}
