6ab9390acb75f03f07de40b4fd0a3ccb5443e7e1
ben salim <salim.benahben@cgi.com>
2021-11-15 11:54:53 +0100

    methode ReebotModule corrigé

---
+++ src/AzureIoTHub.Portal/Shared/Models/RequestPayload.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class RequestPayload
+    {
+    }
+}
