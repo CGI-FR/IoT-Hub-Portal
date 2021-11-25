0484912bc7ac1e6efe03ab124f72521ef52ed19a
ben salim <salim.benahben@cgi.com>
2021-11-25 14:02:02 +0100

    ...

---
+++ src/AzureIoTHub.Portal.Server.Tests/Services/DevicesServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using AzureIoTHub.Portal.Server.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace AzureIoTHub.Portal.Server.Tests.Services
+{
+    internal class DevicesServiceTest
+    {
+
+        [Test]
+        public void GetAllEdgeDevice_returnValue()
+        {
+            // Arrange
+            var moqService = new Mock<IDevicesService>();
+        }
+    }
+}
