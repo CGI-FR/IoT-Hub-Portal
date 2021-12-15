78a03dfc12ad6fe83f7a1068c8efaa1da77bad17
Audrey Serra <audrey.serra@cgi.com>
2021-12-15 11:29:51 +0100

    Ajout de DeviceModelCommandsManager

---
+++ src/AzureIoTHub.Portal/Server/Managers/IDeviceModelCommandsManager.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) CGI France - Grand Est. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Managers
+{
+    using System.Collections.Generic;
+    using AzureIoTHub.Portal.Shared.Models.Device;
+
+    public interface IDeviceModelCommandsManager
+    {
+        List<Command> RetrieveCommands(string deviceModel);
+    }
+}
