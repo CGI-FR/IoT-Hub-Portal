d33579c68282353d756dee6a65a6e6b9be533c4e
ben salim <salim.benahben@cgi.com>
2022-02-07 15:28:43 +0100

    add create concentrator page with required properties

---
+++ src/AzureIoTHub.Portal/Shared/Models/Concentrator/Concentrator.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models.Concentrator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Concentrator
+    {
+        [Required]
+        [RegularExpression("^[A-F0-9]{16}$", ErrorMessage = "DeviceID must contain 16 hexadecimal characters (numbers from 0 to 9 and/or letters from A to F)")]
+        public string DeviceId { get; set; }
+
+        [Required]
+        public string DeviceFriendlyName { get; set; }
+
+        [Required]
+        public string LoraRegion { get; set; }
+
+        public string DeviceType { get; set; }
+
+        public string ClientCertificateThumbprint { get; set; }
+    }
+}
