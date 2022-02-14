fc9ac700e9d74234d7af452ebfafc86163448a20
Beaugrand, Kevin <kevin.beaugrand@cgi.com>
2022-02-14 19:38:12 +0100

    Change comment on device model command

--- src/AzureIoTHub.Portal/Shared/Models/v1.0/DeviceModelCommand.cs
+++ src/AzureIoTHub.Portal/Shared/Models/v1.0/DeviceModelCommand.cs
@@ -7,14 +7,28 @@ namespace AzureIoTHub.Portal.Shared.Models.V10
 
     public class DeviceModelCommand
     {
+        /// <summary>
+        /// The command name.
+        /// </summary>
         [Required(ErrorMessage = "The command name is required.")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The command frame in hexa.
+        /// </summary>
         [Required(ErrorMessage = "The frame is required.")]
         public string Frame { get; set; }
 
+        /// <summary>
+        /// The LoRa WAN port.
+        /// </summary>
         [Required(ErrorMessage = "The port number is required.")]
         [Range(1, 223, ErrorMessage = "The port number should be between 1 and 223.")]
         public int Port { get; set; } = 1;
+
+        /// <summary>
+        /// A value indicating whether this instance is builtin.
+        /// </summary>
+        public bool IsBuiltin { get; set; }
     }
 }
