18466a6c3b282e775b93a6ac20cf0c4896883c6e
Audrey Serra <audrey.serra@cgi.com>
2022-02-21 16:26:38 +0100

    Add DeviceTag model

---
+++ src/AzureIoTHub.Portal/Shared/Models/v1.0/Device/DeviceTag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIoTHub.Portal.Shared.Models.v1._0.Device
+{
+    public class DeviceTag
+    {
+        /// <summary>
+        /// The registered name in the device twin.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The label shown to the user.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Whether the field is required when creating a new device or not.
+        /// </summary>
+        public bool Required { get; set; }
+
+        /// <summary>
+        /// Whether the field can be searcheable via the device search panel or not.
+        /// </summary>
+        public bool Searchable { get; set; }
+    }
+}
