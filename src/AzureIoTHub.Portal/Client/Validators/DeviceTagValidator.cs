5633bba139791ce9a5aad251614a8eb2613d824e
Audrey Serra <audrey.serra@cgi.com>
2022-02-24 15:18:24 +0100

    Added loading circle and form validation

---
+++ src/AzureIoTHub.Portal/Client/Validators/DeviceTagValidator.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Client.Validators
+{
+    using AzureIoTHub.Portal.Shared.Models.v10.Device;
+    using FluentValidation;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DeviceTagValidator : AbstractValidator<DeviceTag>
+    {
+        public DeviceTagValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.Label)
+                .NotEmpty();
+
+        }
+
+        //public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        //{
+        //    var result = await ValidateAsync(ValidationContext<DeviceTag>.CreateWithOptions((DeviceTag)model, x => x.IncludeProperties(propertyName)));
+        //    if (result.IsValid)
+        //        return Array.Empty<string>();
+        //    return result.Errors.Select(e => e.ErrorMessage);
+        //};
+    }
+}
