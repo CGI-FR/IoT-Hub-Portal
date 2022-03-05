b1053b5cfc2323be546e7ab4371ea8b92a5e1f8f
Beaugrand, Kevin <kevin.beaugrand@cgi.com>
2022-03-05 13:27:22 +0100

    Add unit test to Clipboard Service

---
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Services/ClipboardServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using AzureIoTHub.Portal.Client.Services;
+using Microsoft.JSInterop;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using Microsoft.JSInterop.Infrastructure;
+
+namespace AzureIoTHub.Portal.Server.Tests.Unit.Services
+{
+    [TestFixture]
+    public class ClipboardServiceTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<IJSRuntime> mockJSRuntime;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockJSRuntime = this.mockRepository.Create<IJSRuntime>();
+        }
+
+        private ClipboardService CreateService()
+        {
+            return new ClipboardService(
+                this.mockJSRuntime.Object);
+        }
+
+        [Test]
+        public void WriteTextAsync_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var service = this.CreateService();
+            string text = Guid.NewGuid().ToString();
+
+            // Act
+            var result = service.WriteTextAsync(text);
+
+            // Assert
+            this.mockJSRuntime.Verify(c => c.InvokeAsync<IJSVoidResult>(
+                    It.Is<string>(x => x == "navigator.clipboard.writeText"),
+                    It.Is<object?[]>(x => x.Single().ToString() == text)), Times.Once);
+
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
