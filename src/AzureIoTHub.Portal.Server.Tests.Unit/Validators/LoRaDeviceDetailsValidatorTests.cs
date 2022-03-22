4fd449f6151ccb825064b58faabd18198d5001d8
Audrey SERRA <95615798+audserraCGI@users.noreply.github.com>
2022-03-22 15:52:51 +0100

    Visual changes on device details (#474)
    * Moved button and tabs, added expansion panels
    
    * New display on LoRaWAN details page
    
    * Changed tabs' titles
    
    * Fixed validation issues
    
    * Added unit tests on LoraDeviceDetailsValidator
    
    * Removed useless function

---
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Validators/LoRaDeviceDetailsValidatorTests.cs
@@ -0,0 +1,136 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Tests.Unit.Validators
+{
+    using System;
+    using AzureIoTHub.Portal.Client.Validators;
+    using AzureIoTHub.Portal.Models.v10.LoRaWAN;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class LoRaDeviceDetailsValidatorTests
+    {
+        [Test]
+        public void ValidateValidOTAAModel()
+        {
+
+            // Arrange
+            var loraValidator = new LoRaDeviceDetailsValidator();
+            var loraDevice = new LoRaDeviceDetails()
+            {
+                ModelId = Guid.NewGuid().ToString(),
+                UseOTAA = true,
+                AppEUI = Guid.NewGuid().ToString(),
+                AppKey = Guid.NewGuid().ToString()
+            };
+
+            // Act
+            var loraValidation = loraValidator.Validate(loraDevice);
+
+            // Assert
+            Assert.IsTrue(loraValidation.IsValid);
+            Assert.AreEqual(0, loraValidation.Errors.Count);
+        }
+
+        [Test]
+        public void ValidateValidAPBModel()
+        {
+
+            // Arrange
+            var loraValidator = new LoRaDeviceDetailsValidator();
+            var loraDevice = new LoRaDeviceDetails()
+            {
+                ModelId = Guid.NewGuid().ToString(),
+                UseOTAA = false,
+                AppSKey = Guid.NewGuid().ToString(),
+                NwkSKey = Guid.NewGuid().ToString(),
+                DevAddr = Guid.NewGuid().ToString()
+            };
+
+            // Act
+            var loraValidation = loraValidator.Validate(loraDevice);
+
+            // Assert
+            Assert.IsTrue(loraValidation.IsValid);
+            Assert.AreEqual(0, loraValidation.Errors.Count);
+        }
+
+        [Test]
+        public void ValidateMissingModelIdShouldReturnError()
+        {
+            // Arrange
+            var loraValidator = new LoRaDeviceDetailsValidator();
+            var loraDevice = new LoRaDeviceDetails()
+            {
+                UseOTAA = false,
+                AppSKey = Guid.NewGuid().ToString(),
+                NwkSKey = Guid.NewGuid().ToString(),
+                DevAddr = Guid.NewGuid().ToString()
+            };
+
+            // Act
+            var loraValidation = loraValidator.Validate(loraDevice);
+
+            // Assert
+            Assert.IsFalse(loraValidation.IsValid);
+            Assert.AreEqual(1, loraValidation.Errors.Count);
+            Assert.AreEqual(loraValidation.Errors[0].ErrorMessage, "ModelId is required.");
+        }
+
+        [TestCase("AppSKey", "", "NwkSKeyValue", "DevAddrValue")]
+        [TestCase("NwkSKey", "AppSKeyValue", "", "DevAddrValue")]
+        [TestCase("DevAddr", "AppSKeyValue", "NwkSKeyValue", "")]
+        public void ValidateMissingAPBFieldShouldReturnError(
+            string testedValue,
+            string AppSkeyValue,
+            string NwkSKeyValue,
+            string DevAddrValue)
+        {
+            // Arrange
+            var loraValidator = new LoRaDeviceDetailsValidator();
+            var loraDevice = new LoRaDeviceDetails()
+            {
+                ModelId = Guid.NewGuid().ToString(),
+                UseOTAA = false,
+                AppSKey = AppSkeyValue,
+                NwkSKey = NwkSKeyValue,
+                DevAddr = DevAddrValue
+            };
+
+            // Act
+            var loraValidation = loraValidator.Validate(loraDevice);
+
+            // Assert
+            Assert.IsFalse(loraValidation.IsValid);
+            Assert.AreEqual(1, loraValidation.Errors.Count);
+            Assert.AreEqual(loraValidation.Errors[0].ErrorMessage, $"{testedValue} is required.");
+        }
+
+        [TestCase("AppEUI", "", "AppKeyValue")]
+        [TestCase("AppKey", "AppEUIValue", "")]
+        public void ValidateMissingOTAAFieldShouldReturnError(
+            string testedValue,
+            string AppEUIValue,
+            string AppKeyValue)
+        {
+            // Arrange
+            var loraValidator = new LoRaDeviceDetailsValidator();
+            var loraDevice = new LoRaDeviceDetails()
+            {
+                ModelId = Guid.NewGuid().ToString(),
+                UseOTAA = true,
+                AppEUI = AppEUIValue,
+                AppKey = AppKeyValue
+            };
+
+            // Act
+            var loraValidation = loraValidator.Validate(loraDevice);
+
+            // Assert
+            Assert.IsFalse(loraValidation.IsValid);
+            Assert.AreEqual(1, loraValidation.Errors.Count);
+            Assert.AreEqual(loraValidation.Errors[0].ErrorMessage, $"{testedValue} is required.");
+        }
+    }
+}
--- src/AzureIoTHub.Portal/Client/Validators/LoRaDeviceDetailsValidator.cs
+++ src/AzureIoTHub.Portal/Client/Validators/LoRaDeviceDetailsValidator.cs
@@ -3,10 +3,10 @@
 
 namespace AzureIoTHub.Portal.Client.Validators
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
+    //using System;
+    //using System.Collections.Generic;
+    //using System.Linq;
+    //using System.Threading.Tasks;
     using AzureIoTHub.Portal.Models.v10.LoRaWAN;
     using FluentValidation;
 
@@ -14,24 +14,38 @@ namespace AzureIoTHub.Portal.Client.Validators
     {
         public LoRaDeviceDetailsValidator()
         {
+            _ = RuleFor(x => x.ModelId)
+                .NotEmpty()
+                .WithMessage("ModelId is required.");
+
+            // OTAA Settings
+
             _ = RuleFor(x => x.AppEUI)
                 .NotEmpty()
-                .When(x => x.UseOTAA);
+                .When(x => x.UseOTAA)
+                .WithMessage("AppEUI is required.");
 
             _ = RuleFor(x => x.AppKey)
                 .NotEmpty()
-                .When(x => x.UseOTAA);
+                .When(x => x.UseOTAA)
+                .WithMessage("AppKey is required.");
 
-            _ = RuleFor(x => x.ModelId)
-                .NotEmpty();
+            // APB Settings
+
+            _ = RuleFor(x => x.AppSKey)
+                .NotEmpty()
+                .When(x => !x.UseOTAA)
+                .WithMessage("AppSKey is required.");
+
+            _ = RuleFor(x => x.NwkSKey)
+                .NotEmpty()
+                .When(x => !x.UseOTAA)
+                .WithMessage("NwkSKey is required.");
+
+            _ = RuleFor(x => x.DevAddr)
+                .NotEmpty()
+                .When(x => !x.UseOTAA)
+                .WithMessage("DevAddr is required.");
         }
-
-        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-        {
-            var result = await ValidateAsync(ValidationContext<LoRaDeviceDetails>.CreateWithOptions((LoRaDeviceDetails)model, x => x.IncludeProperties(propertyName)));
-            if (result.IsValid)
-                return Array.Empty<string>();
-            return result.Errors.Select(e => e.ErrorMessage);
-        };
     }
 }
