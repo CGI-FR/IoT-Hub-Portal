6a5df09ca1b7d9630555707eab1b055c6a833351
Audrey Serra <audrey.serra@cgi.com>
2022-03-24 15:40:33 +0100

    Fixed property valdiation when fields are empty

--- src/AzureIoTHub.Portal/Client/Validators/DevicePropertyValidator.cs
+++ src/AzureIoTHub.Portal/Client/Validators/DevicePropertyValidator.cs
@@ -29,10 +29,18 @@ namespace AzureIoTHub.Portal.Client.Validators
             _ = RuleForEach(x => x)
                 .ChildRules(c =>
                 {
-                    _ = c.RuleFor(c => c.DisplayName).NotNull();
-                    _ = c.RuleFor(c => c.Name).NotNull();
-                    _ = c.RuleFor(c => c.PropertyType).NotNull();
-                    _ = c.RuleFor(c => c.IsWritable).NotNull();
+                    _ = c.RuleFor(c => c.DisplayName)
+                        .NotNull().WithMessage("Property DisplayName is required.")
+                        .NotEmpty().WithMessage("Property DisplayName is required.");
+                    _ = c.RuleFor(c => c.Name)
+                        .NotNull().WithMessage("Property Name is required.")
+                        .NotEmpty().WithMessage("Property Name is required.");
+                    _ = c.RuleFor(c => c.PropertyType)
+                        .NotNull()
+                        .WithMessage("Property PropertyType is required.");
+                    _ = c.RuleFor(c => c.IsWritable)
+                        .NotNull()
+                        .WithMessage("Property IsWritable is required.");
                 });
         }
     }
