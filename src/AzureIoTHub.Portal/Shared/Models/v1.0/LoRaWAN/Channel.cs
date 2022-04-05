f5c3caaf2f0d942c26496598f18ff4736cc350de
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2022-04-05 18:41:40 +0200

    Fix #544 - Serialize & Deserialize concentrator router config properly

--- src/AzureIoTHub.Portal/Shared/Models/v1.0/LoRaWAN/Channel.cs
+++ src/AzureIoTHub.Portal/Shared/Models/v1.0/LoRaWAN/Channel.cs
@@ -3,6 +3,7 @@
 
 namespace AzureIoTHub.Portal.Models.v10.LoRaWAN
 {
+    using System.Text.Json.Serialization;
     using Newtonsoft.Json;
 
     /// <summary>
@@ -13,32 +14,43 @@ namespace AzureIoTHub.Portal.Models.v10.LoRaWAN
         /// <summary>
         /// A value indicating whether the channel is enabled.
         /// </summary>
+        [JsonPropertyName("enable")]
+        [JsonProperty("enable")]
         public bool? Enable { get; set; }
 
         /// <summary>
         /// The frequency.
         /// </summary>
+        [JsonPropertyName("freq")]
+        [JsonProperty("freq")]
         public int Freq { get; set; }
 
         /// <summary>
         /// The radio.
         /// </summary>
+        [JsonPropertyName("radio")]
+        [JsonProperty("radio")]
         public int Radio { get; set; }
 
         /// <summary>
         /// The interface.
         /// </summary>
+        [JsonPropertyName("if")]
+        [JsonProperty("if")]
         public int If { get; set; }
 
         /// <summary>
         /// The bandwidth.
         /// </summary>
+        [JsonPropertyName("bandwidth")]
+        [JsonProperty("bandwidth")]
         public int Bandwidth { get; set; }
 
         /// <summary>
         /// The spread factor.
         /// </summary>
-        [JsonProperty("Spread_factor")]
+        [JsonPropertyName("spread_factor")]
+        [JsonProperty("spread_factor")]
         public int SpreadFactor { get; set; }
     }
 }
