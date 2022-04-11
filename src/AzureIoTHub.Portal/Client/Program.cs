f24a39b966f4ee05d6f309921c172a3d1c3f8a64
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2022-04-11 20:24:33 +0200

    Fix #420 - Make NavMenu Group items + add collapsible feature (#572)
    * Fix #420 - Make NavMenu Group items + add collapsible feature
    
    * Add unit tests to LocalStorageService
    
    * Use Blazored.LocalStorage + add unit tests

--- src/AzureIoTHub.Portal/Client/Program.cs
+++ src/AzureIoTHub.Portal/Client/Program.cs
@@ -10,6 +10,7 @@ namespace AzureIoTHub.Portal.Client
     using AzureIoTHub.Portal.Client.Services;
     using AzureIoTHub.Portal.Models.v10;
     using AzureIoTHub.Portal.Settings;
+    using Blazored.LocalStorage;
     using Blazored.Modal;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
@@ -34,11 +35,12 @@ namespace AzureIoTHub.Portal.Client
             _ = builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            _ = builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("api"));
-            _ = builder.Services.AddBlazoredModal();
+            _ = builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("api"))
+                .AddScoped<ClipboardService>();
 
+            _ = builder.Services.AddBlazoredLocalStorage();
+            _ = builder.Services.AddBlazoredModal();
             _ = builder.Services.AddMudServices();
-            _ = builder.Services.AddScoped<ClipboardService>();
 
             await ConfigureOidc(builder);
             await ConfigurePortalSettings(builder);
