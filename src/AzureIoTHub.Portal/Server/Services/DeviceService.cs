fb3def10c4205c058da99e91370c09799802773b
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-04-11 19:18:42 +0200

    Use the correct tag 'type' on GetAllDevice

--- src/AzureIoTHub.Portal/Server/Services/DeviceService.cs
+++ src/AzureIoTHub.Portal/Server/Services/DeviceService.cs
@@ -129,12 +129,12 @@ namespace AzureIoTHub.Portal.Server.Services
 
             if (!string.IsNullOrWhiteSpace(filterDeviceType))
             {
-                filter += $" AND devices.tags.deviceType = '{ filterDeviceType }'";
+                filter += $" AND devices.tags.type = '{ filterDeviceType }'";
             }
 
             if (!string.IsNullOrWhiteSpace(excludeDeviceType))
             {
-                filter += $" AND (NOT is_defined(tags.deviceType) OR devices.tags.deviceType != '{ excludeDeviceType }')";
+                filter += $" AND (NOT is_defined(tags.type) OR devices.tags.type != '{ excludeDeviceType }')";
             }
 
             if (!string.IsNullOrWhiteSpace(searchText))
