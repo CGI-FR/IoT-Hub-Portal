76f9477ceb6b4e6f467e7533db6ebe8bda3df565
ben salim <salim.benahben@cgi.com>
2022-04-20 11:54:11 +0200

    configTest

--- src/AzureIoTHub.Portal.Server.Tests.Unit/Helpers/ConfigHelperTest.cs
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Helpers/ConfigHelperTest.cs
@@ -3,40 +3,96 @@
 
 namespace AzureIoTHub.Portal.Server.Tests.Unit.Helpers
 {
+    using System;
     using System.Collections.Generic;
     using AzureIoTHub.Portal.Server.Helpers;
     using Microsoft.Azure.Devices;
-    using Moq;
     using NUnit.Framework;
 
     [TestFixture]
     public class ConfigHelperTest
     {
+        //[Test]
+        //[TestCase("targetedCount")]
+        //[TestCase("appliedCount")]
+        //public void RetrieveMetricValue(string metricName)
+        //{
+        //    //Arrange
+        //    var metricValues = new Dictionary<string, long>
+        //    {
+        //        { "targetedCount", 2 },
+        //        { "appliedCount", 2 }
+        //    };
+
+        //    var configMetric = new ConfigurationMetrics(){ Results = metricValues };
+
+        //    //var config = new Configuration("test"){ SystemMetrics = configMetric };
+
+        //    var mockConfig = new Mock<Configuration>(MockBehavior.Strict);
+        //    mockConfig.SetupProperty(x => x.SystemMetrics).SetReturnsDefault(configMetric);
+
+        //    // Act
+        //    var result = ConfigHelper.RetrieveMetricValue(mockConfig.Object, metricName);
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(2, result);
+        //}
+
         [Test]
-        [TestCase("targetedCount")]
-        [TestCase("appliedCount")]
-        public void RetrieveMetricValue(string metricName)
+        public void CreateDeviceConfigShouldReturnValue()
         {
-            //Arrange
-            var metricValues = new Dictionary<string, long>
+            // Arrange
+            var modelId = Guid.NewGuid().ToString();
+            var targetCondition = $"tags.modelId = '{modelId}' and tags.name = test";
+
+            var config = new Configuration("test")
             {
-                { "targetedCount", 2 },
-                { "appliedCount", 2 }
+                TargetCondition = targetCondition,
+                Labels = new Dictionary<string, string> { { "id", "test" } },
+                Priority = 1
             };
 
-            var configMetric = new ConfigurationMetrics(){ Results = metricValues };
-
-            //var config = new Configuration("test"){ SystemMetrics = configMetric };
-
-            var mockConfig = new Mock<Configuration>(MockBehavior.Strict);
-            mockConfig.SetupProperty(x => x.SystemMetrics).SetReturnsDefault(configMetric);
-
             // Act
-            var result = ConfigHelper.RetrieveMetricValue(mockConfig.Object, metricName);
+            var result = ConfigHelper.CreateDeviceConfig(config);
 
             // Assert
             Assert.IsNotNull(result);
-            Assert.AreEqual(2, result);
+            Assert.AreEqual(config.Priority, result.Priority);
+            Assert.AreEqual(modelId, result.ModelId);
+        }
+
+        [Test]
+        public void CreateDeviceConfigWithNullArgumentShouldThrowException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() => ConfigHelper.CreateDeviceConfig(config: null));
+            Assert.IsNotNull(argumentNullException);
+            Assert.AreEqual("config", argumentNullException.ParamName);
+            Assert.IsInstanceOf<ArgumentNullException>(argumentNullException);
+        }
+
+        [Test]
+        public void CreateDeviceConfigWithNullTargetConditionShouldShouldThrowAnException()
+        {
+            // Arrange
+            var config = new Configuration("test")
+            {
+                TargetCondition = string.Empty,
+                Labels = new Dictionary<string, string> { { "id", "test" } },
+                Priority = 1
+            };
+
+            // Act
+
+            // Assert
+            var invalidOperationException =  Assert.Throws<InvalidOperationException>(() => ConfigHelper.CreateDeviceConfig(config));
+            Assert.IsNotNull(invalidOperationException);
+            Assert.IsInstanceOf<InvalidOperationException>(invalidOperationException);
         }
     }
 }
