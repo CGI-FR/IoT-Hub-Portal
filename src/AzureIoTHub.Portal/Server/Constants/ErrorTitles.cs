c5ce69ee31dc57c197582707f35b1024c4023f0d
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-05-02 19:51:56 +0200

    Add problem details configuration #588

---
+++ src/AzureIoTHub.Portal/Server/Constants/ErrorTitles.cs
@@ -0,0 +1,10 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Constants
+{
+    public static class ErrorTitles
+    {
+        public const string InternalServerError = "Internal Server Error";
+    }
+}
