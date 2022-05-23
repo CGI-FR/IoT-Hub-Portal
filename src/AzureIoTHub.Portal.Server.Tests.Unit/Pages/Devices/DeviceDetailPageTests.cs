7ee3c006f5be2d97b0fd847b636231329f19ac51
Audrey Serra <audrey.serra@cgi.com>
2022-05-23 10:34:44 +0200

    Add DeviceDtailPageTests : ClickOnSaveShouldDisplaySnackbarIfValidationError()

--- src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/Devices/DeviceDetailPageTests.cs
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/Devices/DeviceDetailPageTests.cs
@@ -30,6 +30,7 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages
         private MockHttpMessageHandler mockHttpClient;
         private MockRepository mockRepository;
         private Mock<IDialogService> mockDialogService;
+        private Mock<ISnackbar> mockSnackbarService;
         private FakeNavigationManager mockNavigationManager;
 
         private static string ApiBaseUrl => "/api/devices";
@@ -40,11 +41,14 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages
             this.testContext = new Bunit.TestContext();
 
             this.mockRepository = new MockRepository(MockBehavior.Strict);
-            this.mockDialogService = this.mockRepository.Create<IDialogService>();
-
             this.mockHttpClient = this.testContext.Services.AddMockHttpClient();
 
+            this.mockDialogService = this.mockRepository.Create<IDialogService>();
             _ = this.testContext.Services.AddSingleton(this.mockDialogService.Object);
+
+            this.mockSnackbarService = this.mockRepository.Create<ISnackbar>();
+            _ = this.testContext.Services.AddSingleton(this.mockSnackbarService.Object);
+
             _ = this.testContext.Services.AddMudServices();
 
             _ = this.testContext.Services.AddSingleton(new PortalSettings { IsLoRaSupported = false });
@@ -190,6 +194,95 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages
             this.mockHttpClient.VerifyNoOutstandingExpectation();
         }
 
+        [Test]
+        public void ClickOnSaveShouldDisplaySnackbarIfValidationError()
+        {
+            var mockDeviceModel = new DeviceModel
+            {
+                ModelId = Guid.NewGuid().ToString(),
+                Description = Guid.NewGuid().ToString(),
+                SupportLoRaFeatures = false,
+                Name = Guid.NewGuid().ToString()
+            };
+
+            var mockTag = new DeviceTag
+            {
+                Label = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString(),
+                Required = false,
+                Searchable = false
+            };
+
+            var mockDeviceDetails = new DeviceDetails
+            {
+                DeviceName = Guid.NewGuid().ToString(),
+                ModelId = mockDeviceModel.ModelId,
+                DeviceID = Guid.NewGuid().ToString(),
+                Tags = new Dictionary<string, string>()
+                {
+                    {mockTag.Name,Guid.NewGuid().ToString()}
+                }
+            };
+
+
+            _ = this.mockHttpClient.When(HttpMethod.Put, $"{ApiBaseUrl}")
+                .With(m =>
+                {
+                    Assert.IsAssignableFrom<JsonContent>(m.Content);
+                    var jsonContent = m.Content as JsonContent;
+
+                    Assert.IsAssignableFrom<DeviceDetails>(jsonContent.Value);
+                    var deviceDetails = jsonContent.Value as DeviceDetails;
+
+                    Assert.AreEqual(mockDeviceDetails.DeviceID, deviceDetails.DeviceID);
+                    Assert.AreEqual(mockDeviceDetails.DeviceName, deviceDetails.DeviceName);
+                    Assert.AreEqual(mockDeviceDetails.ModelId, deviceDetails.ModelId);
+
+                    return true;
+                })
+                .RespondText(string.Empty);
+
+            _ = this.mockHttpClient.When(HttpMethod.Get, $"/api/devices/{mockDeviceDetails.DeviceID}")
+                .RespondJson(mockDeviceDetails);
+
+            _ = this.mockHttpClient.When(HttpMethod.Get, $"/api/models/{mockDeviceDetails.ModelId}")
+                .RespondJson(mockDeviceModel);
+
+            _ = this.mockHttpClient.When(HttpMethod.Get, $"/api/settings/device-tags")
+                .RespondJson(new List<DeviceTag>()
+                {
+                    mockTag
+                });
+
+            _ = this.mockHttpClient.When(HttpMethod.Get, $"{ApiBaseUrl}/{mockDeviceDetails.DeviceID}/properties")
+                .RespondJson(Array.Empty<DeviceProperty>());
+
+            _ = this.mockHttpClient.When(HttpMethod.Post, $"{ApiBaseUrl}/{mockDeviceDetails.DeviceID}/properties")
+                .RespondText(string.Empty);
+
+            var cut = RenderComponent<DeviceDetailPage>(ComponentParameter.CreateParameter("DeviceID", mockDeviceDetails.DeviceID));
+            Thread.Sleep(2500);
+
+            cut.Find($"#{nameof(DeviceDetails.DeviceName)}").Change("");
+            var saveButton = cut.WaitForElement("#saveButton");
+
+            var mockDialogReference = new DialogReference(Guid.NewGuid(), this.mockDialogService.Object);
+
+            _ = this.mockDialogService.Setup(c => c.Show<ProcessingDialog>("Processing", It.IsAny<DialogParameters>()))
+                .Returns(mockDialogReference);
+
+            _ = this.mockDialogService.Setup(c => c.Close(It.Is<DialogReference>(x => x == mockDialogReference)));
+
+            _ = this.mockSnackbarService.Setup(c => c.Add("One or more validation errors occurred", Severity.Error, null));
+
+            // Act
+            saveButton.Click();
+            Thread.Sleep(2500);
+
+            // Assert            
+            this.mockHttpClient.VerifyNoOutstandingExpectation();
+            this.mockRepository.VerifyAll();
+        }
 
         public void Dispose()
         {
