389eae3ee6ede41845cdfad79e58d22c58353c8f
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-06-22 20:11:51 +0200

    Add device metrics loader/exporter #376

---
+++ src/AzureIoTHub.Portal/Server/Services/DeviceMetricExporterService.cs
@@ -0,0 +1,46 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Services
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Constants;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Prometheus;
+    using Shared.Models.v1._0;
+
+    public class DeviceMetricExporterService : BackgroundService
+    {
+        private readonly ILogger<DeviceMetricExporterService> logger;
+        private readonly ConfigHandler configHandler;
+        private readonly PortalMetric portalMetric;
+
+        private readonly Counter deviceCounter = Metrics.CreateCounter(MetricName.DeviceCount, "Devices count");
+        private readonly Counter connectedDeviceCounter = Metrics.CreateCounter(MetricName.ConnectedDeviceCount, "Connected devices count");
+
+        public DeviceMetricExporterService(ILogger<DeviceMetricExporterService> logger, ConfigHandler configHandler, PortalMetric portalMetric)
+        {
+            this.logger = logger;
+            this.configHandler = configHandler;
+            this.portalMetric = portalMetric;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var timer = new PeriodicTimer(TimeSpan.FromSeconds(this.configHandler.MetricExporterRefreshIntervalInSeconds));
+
+            do
+            {
+                this.logger.LogInformation("Start exporting devices metrics");
+
+                this.deviceCounter.IncTo(this.portalMetric.DeviceCount);
+                this.connectedDeviceCounter.IncTo(this.portalMetric.ConnectedDeviceCount);
+
+                this.logger.LogInformation("End exporting devices metrics");
+            } while (await timer.WaitForNextTickAsync(stoppingToken));
+        }
+    }
+}
