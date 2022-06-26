585cedbf0113b5224a0bcf2c4c028708f3f3872b
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-06-26 22:42:13 +0200

    Update ErrorTests to implement BlazorUnitTest #868

--- src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/Shared/ErrorTests.cs
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/Shared/ErrorTests.cs
@@ -5,44 +5,30 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.Shared
 {
     using System;
     using System.Collections.Generic;
-    using Bunit;
     using Client.Exceptions;
     using Client.Models;
     using Client.Shared;
-    using Microsoft.AspNetCore.Components;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
     using Moq;
     using MudBlazor;
-    using MudBlazor.Services;
     using NUnit.Framework;
 
     [TestFixture]
-    public class ErrorTests : IDisposable
+    public class ErrorTests : BlazorUnitTest
     {
-        private Bunit.TestContext testContext;
-        private MockRepository mockRepository;
-        private Mock<ISnackbar> mockSnackbar;
+        private Mock<ISnackbar> mockSnackBar;
         private Mock<IWebAssemblyHostEnvironment> mockWebAssemblyHostEnvironment;
 
-        [SetUp]
-        public void SetUp()
+        public override void Setup()
         {
-            this.testContext = new Bunit.TestContext();
+            base.Setup();
 
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-            this.mockSnackbar = new Mock<ISnackbar>();
-            this.mockWebAssemblyHostEnvironment = this.mockRepository.Create<IWebAssemblyHostEnvironment>();
+            this.mockSnackBar = MockRepository.Create<ISnackbar>();
+            this.mockWebAssemblyHostEnvironment = MockRepository.Create<IWebAssemblyHostEnvironment>();
 
-            _ = this.testContext.Services.AddSingleton(this.mockWebAssemblyHostEnvironment.Object);
-
-            _ = this.testContext.Services.AddMudServices();
-        }
-
-        private IRenderedComponent<TComponent> RenderComponent<TComponent>(params ComponentParameter[] parameters)
-            where TComponent : IComponent
-        {
-            return this.testContext.RenderComponent<TComponent>(parameters);
+            _ = Services.AddSingleton(this.mockWebAssemblyHostEnvironment.Object);
+            _ = Services.AddSingleton(this.mockSnackBar.Object);
         }
 
         [Test]
@@ -58,8 +44,9 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.Shared
                 ExceptionDetails = new List<ProblemDetailsWithExceptionDetails.ExceptionDetail>()
             });
 
-            _ = this.mockSnackbar.Setup(c => c.Add(It.Is<string>(x => x == problemDetailsException.ProblemDetailsWithExceptionDetails.Detail),
-                It.Is<Severity>(severity => severity == Severity.Error), It.IsAny<Action<SnackbarOptions>>()));
+            _ = this.mockSnackBar.Setup(c => c.Add(It.Is<string>(x => x == problemDetailsException.ProblemDetailsWithExceptionDetails.Detail),
+                It.Is<Severity>(severity => severity == Severity.Error), It.IsAny<Action<SnackbarOptions>>()))
+                .Returns(this.mockSnackBar.Object as Snackbar);
 
             var cut = RenderComponent<Error>();
 
@@ -67,18 +54,7 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.Shared
             cut.Instance.ProcessProblemDetails(problemDetailsException);
 
             // Assert
-            this.mockRepository.VerifyAll();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            this.testContext?.Dispose();
+            MockRepository.VerifyAll();
         }
     }
 }
