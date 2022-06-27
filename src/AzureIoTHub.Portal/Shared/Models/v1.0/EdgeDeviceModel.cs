ac9af4ba042557875a0a1dd31239c6b44d99fc71
ben salim <salim.benahben@cgi.com>
2022-06-27 16:45:45 +0200

    edge model create page

---
+++ src/AzureIoTHub.Portal/Shared/Models/v1.0/EdgeDeviceModel.cs
@@ -0,0 +1,13 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Shared.Models.v10
+{
+
+    public class EdgeDeviceModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
