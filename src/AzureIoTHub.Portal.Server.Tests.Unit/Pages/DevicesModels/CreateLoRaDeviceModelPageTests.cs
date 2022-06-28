374530dbd3204db37875f5693f4c2f83a27a65e4
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-06-28 20:25:56 +0200

    Update CreateLoRaDeviceModelPageTests to implement BlazorUnitTest #868

--- src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/DevicesModels/CreateLoRaDeviceModelPageTests.cs
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/DevicesModels/CreateLoRaDeviceModelPageTests.cs
@@ -3,58 +3,23 @@
 
 namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.DevicesModels
 {
-    using System;
-    using System.Collections.Generic;
-    using AzureIoTHub.Portal.Server.Tests.Unit.Helpers;
-    using Bunit;
     using Microsoft.Extensions.DependencyInjection;
     using Moq;
     using MudBlazor;
-    using MudBlazor.Interop;
-    using MudBlazor.Services;
     using NUnit.Framework;
-    using RichardSzalay.MockHttp;
 
     [TestFixture]
-    public class CreateLoRaDeviceModelPageTests : IDisposable
+    public class CreateLoRaDeviceModelPageTests : BlazorUnitTest
     {
-        private Bunit.TestContext testContext;
-        private MockHttpMessageHandler mockHttpClient;
-
-        private MockRepository mockRepository;
         private Mock<IDialogService> mockDialogService;
 
-        [SetUp]
-        public void SetUp()
+        public override void Setup()
         {
-            this.testContext = new Bunit.TestContext();
+            base.Setup();
 
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-            this.mockDialogService = this.mockRepository.Create<IDialogService>();
-            this.mockHttpClient = this.testContext.Services
-                                            .AddMockHttpClient();
+            this.mockDialogService = MockRepository.Create<IDialogService>();
 
-            _ = this.testContext.Services.AddSingleton(this.mockDialogService.Object);
-
-            _ = this.testContext.Services.AddMudServices();
-
-            _ = this.testContext.JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
-            _ = this.testContext.JSInterop.SetupVoid("mudPopover.connect", _ => true);
-            _ = this.testContext.JSInterop.SetupVoid("Blazor._internal.InputFile.init", _ => true);
-            _ = this.testContext.JSInterop.Setup<BoundingClientRect>("mudElementRef.getBoundingClientRect", _ => true);
-            _ = this.testContext.JSInterop.Setup<IEnumerable<BoundingClientRect>>("mudResizeObserver.connect", _ => true);
-
-            this.mockHttpClient.AutoFlush = true;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
+            _ = Services.AddSingleton(this.mockDialogService.Object);
         }
     }
 }
