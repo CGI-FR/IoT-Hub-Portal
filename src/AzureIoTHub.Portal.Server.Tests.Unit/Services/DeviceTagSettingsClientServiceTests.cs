bf0184f40e0f13a61596ac1a80d51bea02d94786
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-07-03 16:41:36 +0200

    Add DeviceTagSettingsClientServiceTests

---
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Services/DeviceTagSettingsClientServiceTests.cs
@@ -0,0 +1,50 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Tests.Unit.Services
+{
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using AzureIoTHub.Portal.Client.Services;
+    using FluentAssertions;
+    using Helpers;
+    using Microsoft.Extensions.DependencyInjection;
+    using Models.v10;
+    using NUnit.Framework;
+    using RichardSzalay.MockHttp;
+
+    [TestFixture]
+    public class DeviceTagSettingsClientServiceTests : BlazorUnitTest
+    {
+        private IDeviceTagSettingsClientService deviceTagSettingsClientService;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            _ = Services.AddSingleton<IDeviceTagSettingsClientService, DeviceTagSettingsClientService>();
+
+            this.deviceTagSettingsClientService = Services.GetRequiredService<IDeviceTagSettingsClientService>();
+        }
+
+        [Test]
+        public async Task GetDeviceTagsShouldReturnDeviceTags()
+        {
+            // Arrange
+            var expectedDeviceTags = Fixture.Build<DeviceTag>().CreateMany(3).ToList();
+
+            _ = MockHttpClient.When(HttpMethod.Get, "/api/settings/device-tags")
+                .RespondJson(expectedDeviceTags);
+
+            // Act
+            var result = await this.deviceTagSettingsClientService.GetDeviceTags();
+
+            // Assert
+            _ = result.Should().BeEquivalentTo(expectedDeviceTags);
+            MockHttpClient.VerifyNoOutstandingRequest();
+            MockHttpClient.VerifyNoOutstandingExpectation();
+        }
+    }
+}
