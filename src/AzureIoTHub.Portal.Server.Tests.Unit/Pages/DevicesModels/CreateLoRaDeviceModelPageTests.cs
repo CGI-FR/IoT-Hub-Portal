4d306814718217aa9a0afd8b47f4d28bb1e27416
Audrey Serra <audrey.serra@cgi.com>
2022-07-20 16:01:14 +0200

    Add tests on CreateLoRaDeviceModel

--- src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/DevicesModels/CreateLoRaDeviceModelPageTests.cs
+++ src/AzureIoTHub.Portal.Server.Tests.Unit/Pages/DevicesModels/CreateLoRaDeviceModelPageTests.cs
@@ -3,6 +3,12 @@
 
 namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.DevicesModels
 {
+    using System;
+    using System.Collections.Generic;
+    using AzureIoTHub.Portal.Client.Pages.DeviceModels.LoRaWAN;
+    using AzureIoTHub.Portal.Models.v10.LoRaWAN;
+    using Bunit;
+    using FluentAssertions;
     using Microsoft.Extensions.DependencyInjection;
     using Moq;
     using MudBlazor;
@@ -21,5 +27,82 @@ namespace AzureIoTHub.Portal.Server.Tests.Unit.Pages.DevicesModels
 
             _ = Services.AddSingleton(this.mockDialogService.Object);
         }
+
+        [Test]
+        public void CreateLoRaDeviceModelPageShouldBeRenderedProperly()
+        {
+            // Arrange
+            var model = new LoRaDeviceModel();
+            var commands = new List<DeviceModelCommand>();
+            var commandValidation = new List<MudForm>()
+            {
+                new MudForm(),
+                new MudForm(),
+                new MudForm()
+            };
+
+            var cut = RenderComponent<CreateLoraDeviceModel>(
+                ComponentParameter.CreateParameter("LoRaDeviceModel", model),
+                ComponentParameter.CreateParameter("Commands", commands),
+                ComponentParameter.CreateParameter("CommandValidation", commandValidation)
+            );
+
+            var panels = cut.WaitForElements(".mud-expand-panel");
+            cut.WaitForAssertion(() => cut.FindAll(".mud-expand-panel").Count.Should().Be(3));
+        }
+
+        [Test]
+        public void ClickOnAddShouldAddRow()
+        {
+            // Arrange
+            var model = new LoRaDeviceModel();
+            var commands = new List<DeviceModelCommand>();
+            var commandValidation = new List<MudForm>()
+            {
+                new MudForm(),
+                new MudForm(),
+                new MudForm()
+            };
+
+            var cut = RenderComponent<CreateLoraDeviceModel>(
+                ComponentParameter.CreateParameter("LoRaDeviceModel", model),
+                ComponentParameter.CreateParameter("Commands", commands),
+                ComponentParameter.CreateParameter("CommandValidation", commandValidation)
+            );
+
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(0));
+            var addButton = cut.WaitForElement("#addButton");
+            addButton.Click();
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(1));
+        }
+
+        [Test]
+        public void ClickOnRemoveShouldDeleteRow()
+        {
+            // Arrange
+            var model = new LoRaDeviceModel();
+            var commands = new List<DeviceModelCommand>();
+            var commandValidation = new List<MudForm>()
+            {
+                new MudForm(),
+                new MudForm(),
+                new MudForm()
+            };
+
+            var cut = RenderComponent<CreateLoraDeviceModel>(
+                ComponentParameter.CreateParameter("LoRaDeviceModel", model),
+                ComponentParameter.CreateParameter("Commands", commands),
+                ComponentParameter.CreateParameter("CommandValidation", commandValidation)
+            );
+
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(0));
+            var addButton = cut.WaitForElement("#addButton");
+            addButton.Click();
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(1));
+            var removeButton = cut.WaitForElement("#removeButton");
+            removeButton.Click();
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(0));
+        }
+
     }
 }
