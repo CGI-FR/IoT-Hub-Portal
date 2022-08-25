d64d791db96298bf2a8080c6963472525977f9e1
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2022-08-25 12:18:46 +0200

    Feature/add pgsql database connection (#1121)
    * Add entityframework + PGSql nuget packages
    
    * Add PGSql database context + initial database creation

---
+++ src/AzureIoTHub.Portal/Server/Migrations/20220822164454_InitialCreate.cs
@@ -0,0 +1,20 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Server.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
