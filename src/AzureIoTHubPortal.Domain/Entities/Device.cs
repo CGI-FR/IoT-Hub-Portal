c409a70e3265ff2973687c29e7354d51c876999a
ben salim <salim.benahben@cgi.com>
2022-09-19 15:09:16 +0200

    add new entities

---
+++ src/AzureIoTHubPortal.Domain/Entities/Device.cs
@@ -0,0 +1,40 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Domain.Entities
+{
+    using AzureIoTHub.Portal.Domain.Base;
+
+    public class Device : EntityBase
+    {
+        /// <summary>
+        /// The name of the device.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The model identifier.
+        /// </summary>
+        public string DeviceModelId { get; set; }
+
+        /// <summary>
+        ///   <c>true</c> if this instance is connected; otherwise, <c>false</c>.
+        /// </summary>
+        public bool IsConnected { get; set; }
+
+        /// <summary>
+        ///   <c>true</c> if this instance is enabled; otherwise, <c>false</c>.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// The status updated time.
+        /// </summary>
+        public DateTime StatusUpdatedTime { get; set; }
+
+        /// <summary>
+        /// List of custom device tags and their values.
+        /// </summary>
+        public Dictionary<string, string> Tags { get; set; } = new();
+    }
+}
