fe4962d1751302c2d5f75d44f6933d215aeca174
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-10-03 16:32:47 +0200

    Add unit test GetPaginatedListAsync_CustomFilter_ExpectedPageReturned

--- src/AzureIoTHub.Portal.Tests.Unit/Infrastructure/Repositories/GenericRepositoryTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Infrastructure/Repositories/GenericRepositoryTests.cs
@@ -4,10 +4,13 @@
 namespace AzureIoTHub.Portal.Tests.Unit.Infrastructure.Repositories
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using AzureIoTHub.Portal.Domain.Entities;
     using AzureIoTHub.Portal.Infrastructure.Repositories;
     using AzureIoTHub.Portal.Tests.Unit.UnitTests.Bases;
+    using FluentAssertions;
     using Microsoft.EntityFrameworkCore;
     using Moq;
     using NUnit.Framework;
@@ -149,6 +152,85 @@ namespace AzureIoTHub.Portal.Tests.Unit.Infrastructure.Repositories
 
             // Assert
             Assert.IsNull(context.Set<DeviceModelProperty>().Find(entityId));
+        }
+
+
+
+        [Test]
+        public async Task GetPaginatedListAsync_CustomFilter_ExpectedPageReturned()
+        {
+            // Arrange
+            var context = SetupDbContext();
+
+            await context.AddRangeAsync(new List<DeviceTag>
+            {
+                new()
+                {
+                    Id = "device_tag_01",
+                    Label = "Location 1",
+                    Required = false,
+                    Searchable = true
+                },
+                new()
+                {
+                    Id = "device_tag_02",
+                    Label = "Location 2",
+                    Required = true,
+                    Searchable = true
+                },
+                new()
+                {
+                    Id = "device_tag_03",
+                    Label = "Machine",
+                    Required = true,
+                    Searchable = true
+                },
+                new()
+                {
+                    Id = "device_tag_04",
+                    Label = "Location",
+                    Required = false,
+                    Searchable = false
+                },
+            });
+
+            _ = await context.SaveChangesAsync();
+
+            var instance = new GenericRepository<DeviceTag>(context);
+
+            // Order by label desc
+            var orderBy = new[] { $"{nameof(DeviceTag.Label)} desc" };
+
+            // Filter only tags with labels containing "location" and are searchable
+            var deviceTagPredicate = PredicateBuilder.True<DeviceTag>()
+                .And(tag => tag.Label.ToLowerInvariant().Contains("location"))
+                .And(tag => tag.Searchable);
+
+            // Act
+            var result = await instance.GetPaginatedListAsync(0, 10, orderBy, deviceTagPredicate);
+
+            // Assert
+            _ = result.Data.Count.Should().Be(2);
+            _ = result.TotalCount.Should().Be(2);
+            _ = result.PageSize.Should().Be(10);
+            _ = result.HasNextPage.Should().BeFalse();
+            _ = result.HasPreviousPage.Should().BeFalse();
+            _ = result.CurrentPage.Should().Be(0);
+            _ = result.TotalPages.Should().Be(1);
+
+            var firstDeviceTag = result.Data.First();
+
+            _ = firstDeviceTag.Id.Should().Be("device_tag_02");
+            _ = firstDeviceTag.Label.Should().Be("Location 2");
+            _ = firstDeviceTag.Required.Should().BeTrue();
+            _ = firstDeviceTag.Searchable.Should().BeTrue();
+
+            var secondDeviceTag = result.Data.Skip(1).First();
+
+            _ = secondDeviceTag.Id.Should().Be("device_tag_01");
+            _ = secondDeviceTag.Label.Should().Be("Location 1");
+            _ = secondDeviceTag.Required.Should().BeFalse();
+            _ = secondDeviceTag.Searchable.Should().BeTrue();
         }
     }
 }
