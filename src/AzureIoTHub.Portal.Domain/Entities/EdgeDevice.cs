bf8c8736b09776324345ec72823668dcbdef4336
ChristopheRib63 <83826573+ChristopheRib63@users.noreply.github.com>
2022-10-28 12:07:00 +0200

    Task/1317 edge device connection state should be stored on database (#1464)
    * package lock
    
    * Change Edge device
    
    * Test solution
    
    Co-authored-by: crib <christophe.ribeiro@cgi.com>

--- src/AzureIoTHub.Portal.Domain/Entities/EdgeDevice.cs
+++ src/AzureIoTHub.Portal.Domain/Entities/EdgeDevice.cs
@@ -13,6 +13,9 @@ namespace AzureIoTHub.Portal.Domain.Entities
 
         public int Version { get; set; }
 
+        public string ConnectionState { get; set; }
+
+
         /// <summary>
         ///   <c>true</c> if this instance is enabled; otherwise, <c>false</c>.
         /// </summary>
