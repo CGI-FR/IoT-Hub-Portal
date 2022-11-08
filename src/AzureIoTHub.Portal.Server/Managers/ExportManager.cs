00af3c8b2cb18cc02e329ced8b5318c2e484b74d
Audrey Serra <audrey.serra@cgi.com>
2022-11-08 17:43:13 +0100

    WIP :  Changes in the Export function

--- src/AzureIoTHub.Portal.Server/Managers/ExportManager.cs
+++ src/AzureIoTHub.Portal.Server/Managers/ExportManager.cs
@@ -3,56 +3,128 @@
 
 namespace AzureIoTHub.Portal.Server.Managers
 {
+    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Text;
+    using System.Text.Json.Nodes;
     using System.Threading.Tasks;
     using AzureIoTHub.Portal.Infrastructure;
     using AzureIoTHub.Portal.Server.Services;
-    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Logging;
 
     public class ExportManager : IExportManager
     {
         private readonly PortalDbContext portalDbContext;
         private readonly IExternalDeviceService externalDevicesService;
         private readonly IDeviceTagService deviceTagService;
+        private readonly IDeviceModelPropertiesService deviceModelPropertiesService;
+        private readonly ILogger<ExportManager> log;
+        //private readonly PortalSettings portalSettings;
 
         public ExportManager(PortalDbContext portalDbContext,
                                 IExternalDeviceService externalDevicesService,
-                                IDeviceTagService deviceTagService)
+                                IDeviceTagService deviceTagService,
+                                IDeviceModelPropertiesService deviceModelPropertiesService,
+                                //PortalSettings portalSettings
+                                ILogger<ExportManager> log)
         {
             this.portalDbContext = portalDbContext;
             this.externalDevicesService = externalDevicesService;
             this.deviceTagService = deviceTagService;
+            this.deviceModelPropertiesService = deviceModelPropertiesService;
+            this.log = log;
+            //this.portalSettings = portalSettings;
         }
 
 
         public async Task<Stream> ExportDeviceList()
         {
+            var list = await this.externalDevicesService.GetDevicesToExport();
             var tags = this.deviceTagService.GetAllTagsNames();
-            var query = this.portalDbContext.Devices
-                .Include(device => device.Tags);
-            var devices = await query.ToListAsync();
-
-            var textContent = "Id,Name,DeviceModelId,IsEnabled,Version";
-            foreach (var tag in tags)
+            var properties = this.deviceModelPropertiesService.GetAllPropertiesNames() as List<string>;
+            //if (portalSettings.IsLoRaSupported)
+            if (true)
             {
-                textContent += $",TAG:{tag}";
+                properties.AddRange(new List<string>()
+                {
+                    "AppKey",
+                    "AppEUI",
+                    "AppSKey",
+                    "NwkSKey",
+                    "DevAddr",
+                    "GatewayID",
+                    "Downlink",
+                    "ClassType",
+                    "PreferredWindow",
+                    "Deduplication",
+                    "RXDelay",
+                    "ABPRelaxMode",
+                    "SensorDecoder"
+                });
             }
 
-            foreach (var device in devices)
+            var stringBuilder = new StringBuilder("Id,Name,DeviceModelId");
+            _ = stringBuilder.Append(",TAG:");
+            _ = stringBuilder.AppendJoin(",TAG:", tags);
+            _ = stringBuilder.Append(",PROPERTY:");
+            _ = stringBuilder.AppendJoin(",PROPERTY:", properties);
+
+            //
+
+            foreach (var item in list)
             {
-                textContent += $"\n{device.Id},{device.Name},{device.DeviceModelId},{device.IsEnabled},{device.Version}";
+                var deviceObject = JsonNode.Parse(item)!;
+                var deviceTags = deviceObject!["tags"]!;
+
+                //textContent += $"\n{deviceObject["deviceId"]!},{deviceObject["tags"]["deviceName"]!},{deviceObject["tags"]["modelId"]!}";
+                _ = stringBuilder.Append($"\n{deviceObject["deviceId"]!},{deviceObject["tags"]["deviceName"]!},{deviceObject["tags"]["modelId"]!}");
+
                 foreach (var tag in tags)
                 {
-                    var value = device.Tags.Where(x => x.Name == tag).Select(x => x.Value).SingleOrDefault();
-                    textContent += $",{value}";
+                    var value = deviceObject!["tags"][tag];
+                    //textContent += $",{value}";
+                    _ = stringBuilder.Append($",{value}");
+                }
+                //_ = stringBuilder.AppendJoin(',', deviceObject!["tags"][(tag => n tags)]);
+                foreach (var property in properties)
+                {
+                    var value = deviceObject!["desired"][property];
+                    //textContent += $",{value}";
+                    _ = stringBuilder.Append($",{value}");
                 }
             }
+
+            var textContent = stringBuilder.ToString();
 
             var textAsBytes = Encoding.Unicode.GetBytes(textContent);
             var stream = new MemoryStream(textAsBytes);
             return stream;
+
+            //var tags = this.deviceTagService.GetAllTagsNames();
+
+            //var query = this.portalDbContext.Devices
+            //    .Include(device => device.Tags);
+            //var devices = await query.ToListAsync();
+
+            //var textContent = "Id,Name,DeviceModelId,IsEnabled,Version";
+            //foreach (var tag in tags)
+            //{
+            //    textContent += $",TAG:{tag}";
+            //}
+
+            //foreach (var device in devices)
+            //{
+            //    textContent += $"\n{device.Id},{device.Name},{device.DeviceModelId},{device.IsEnabled},{device.Version}";
+            //    foreach (var tag in tags)
+            //    {
+            //        var value = device.Tags.Where(x => x.Name == tag).Select(x => x.Value).SingleOrDefault();
+            //        textContent += $",{value}";
+            //    }
+            //}
+
+            //var textAsBytes = Encoding.Unicode.GetBytes(textContent);
+            //var stream = new MemoryStream(textAsBytes);
+            //return stream;
         }
     }
 }
