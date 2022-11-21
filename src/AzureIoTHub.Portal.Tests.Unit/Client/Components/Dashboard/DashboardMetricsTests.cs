89074fefaa562135371674325da42ecb0d0c040b
crib <christophe.ribeiro@cgi.com>
2022-11-21 16:06:33 +0100

    Move blazor components form pages folder to components folder

--- src/AzureIoTHub.Portal.Tests.Unit/Client/Components/Dashboard/DashboardMetricsTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Components/Dashboard/DashboardMetricsTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) CGI France. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.Dashboard
+namespace AzureIoTHub.Portal.Tests.Unit.Components.Dashboard
 {
     using System.Globalization;
     using AzureIoTHub.Portal.Client.Exceptions;
--- src/AzureIoTHub.Portal.Tests.Unit/Client/Components/Devices/DeviceToDuplicateSelectorTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Components/Devices/DeviceToDuplicateSelectorTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) CGI France. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.Devices
+namespace AzureIoTHub.Portal.Tests.Unit.Components.Devices
 {
     using System.Collections.Generic;
     using AngleSharp.Dom;
