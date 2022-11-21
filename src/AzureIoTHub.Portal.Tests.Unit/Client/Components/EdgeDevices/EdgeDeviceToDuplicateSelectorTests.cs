5bdb9fffebff5bb7f5fc849cd392915d01279640
crib <christophe.ribeiro@cgi.com>
2022-11-21 16:06:33 +0100

    Move blazor components form pages folder to components folder

--- src/AzureIoTHub.Portal.Tests.Unit/Client/Components/EdgeDevices/EdgeDeviceToDuplicateSelectorTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Components/EdgeDevices/EdgeDeviceToDuplicateSelectorTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) CGI France. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.EdgeDevices
+namespace AzureIoTHub.Portal.Tests.Unit.Components.EdgeDevices
 {
     using System.Collections.Generic;
     using AngleSharp.Dom;
