8b9d0049fd81852882a78785e68421c8b390fbf8
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-11-21 19:26:51 +0100

    Add unit test on cancel action on LoRaDeviceTelemetryDialog

--- src/AzureIoTHub.Portal.Tests.Unit/Client/Pages/Devices/LoRaDeviceTelemetryDialogTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Pages/Devices/LoRaDeviceTelemetryDialogTests.cs
@@ -16,6 +16,7 @@ namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.Devices
     using AutoFixture;
     using AzureIoTHub.Portal.Shared.Models.v1._0;
     using System.Linq;
+    using FluentAssertions;
 
     [TestFixture]
     public class LoRaDeviceTelemetryDialogTests : BlazorUnitTest
@@ -57,6 +58,40 @@ namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.Devices
             _ = cut.WaitForElement("canvas");
 
             // Assert
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+
+        [Test]
+        public async Task Show_CancelDialog_DialogIsCancelled()
+        {
+            // Arrange
+            var deviceId = Guid.NewGuid().ToString();
+
+            _ = this.mockLoRaWanDeviceClientService.Setup(service => service.GetDeviceTelemetry(deviceId))
+                .ReturnsAsync(Fixture.CreateMany<LoRaDeviceTelemetryDto>(1).ToList());
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {
+                    "DeviceId", deviceId
+                }
+            };
+
+            IDialogReference dialogReference = null;
+
+            // Act
+            await cut.InvokeAsync(() => dialogReference = service?.Show<LoRaDeviceTelemetryDialog>(string.Empty, parameters));
+            _ = cut.WaitForElement("canvas");
+            var deleteBtn = cut.Find("#cancel-telemetry-dialog");
+            deleteBtn.Click();
+
+            var result = await dialogReference.Result;
+
+            // Assert
+            _ = result.Cancelled.Should().BeTrue();
             cut.WaitForAssertion(() => MockRepository.VerifyAll());
         }
     }
