b97c4ff068fb50632e2aa71dbd2cf36093f2bf82
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2022-11-24 18:57:36 +0100

    Add unit test on DeleteDevicePage

---
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Pages/Devices/DeleteDevicePageTests.cs
@@ -0,0 +1,91 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Tests.Unit.Client.Pages.Devices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+    using AzureIoTHub.Portal.Client.Pages.Devices;
+    using AzureIoTHub.Portal.Client.Services;
+    using AzureIoTHub.Portal.Models.v10;
+    using AzureIoTHub.Portal.Tests.Unit.UnitTests.Bases;
+    using Bunit;
+    using FluentAssertions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Moq;
+    using MudBlazor;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DeleteDevicePageTests : BlazorUnitTest
+    {
+        private Mock<IDeviceClientService> mockDeviceClientService;
+        private Mock<ILoRaWanDeviceClientService> mockLoRaWanDeviceClientService;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            this.mockDeviceClientService = MockRepository.Create<IDeviceClientService>();
+            this.mockLoRaWanDeviceClientService = MockRepository.Create<ILoRaWanDeviceClientService>();
+
+            _ = Services.AddSingleton(this.mockDeviceClientService.Object);
+            _ = Services.AddSingleton(this.mockLoRaWanDeviceClientService.Object);
+        }
+
+        [Test]
+        public async Task DeleteDevice_NonLoRaDevice_DeviceDeleted()
+        {
+            // Arrange
+            var deviceId = Guid.NewGuid().ToString();
+
+            _ = this.mockDeviceClientService.Setup(service => service.DeleteDevice(deviceId))
+                .Returns(Task.CompletedTask);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"deviceID", deviceId},
+            };
+
+            // Act
+            await cut.InvokeAsync(() => service?.Show<DeleteDevicePage>(string.Empty, parameters));
+            cut.WaitForElement("#delete-device").Click();
+
+            // Assert
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+
+        [Test]
+        public async Task DeleteDevice_LoRaDevice_LoRaDeviceDeleted()
+        {
+            // Arrange
+            var deviceId = Guid.NewGuid().ToString();
+
+            _ = this.mockLoRaWanDeviceClientService.Setup(service => service.DeleteDevice(deviceId))
+                .Returns(Task.CompletedTask);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"deviceID", deviceId},
+                {"IsLoRaWan", true}
+            };
+
+            // Act
+            await cut.InvokeAsync(() => service?.Show<DeleteDevicePage>(string.Empty, parameters));
+            cut.WaitForElement("#delete-device").Click();
+
+            // Assert
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+    }
+}
