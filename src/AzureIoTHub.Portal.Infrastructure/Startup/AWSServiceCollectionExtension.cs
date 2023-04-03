0d350f9d78f919b27993e0548ff235ee96753a95
delager <raphael.delage@cgi.com>
2023-04-03 17:52:14 +0200

    #1929 Configuration logic by provider
    - Add CloudProvider dependencies to the Server Startup
    - Add CloudProvider dependencies to the Infrastructure Startup
    - Azure first in the switch to unify

---
+++ src/AzureIoTHub.Portal.Infrastructure/Startup/AWSServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Infrastructure.Startup
+{
+    using AzureIoTHub.Portal.Infrastructure.ServicesHealthCheck;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class AWSServiceCollectionExtension
+    {
+        public static IServiceCollection AddAWSInfrastructureLayer(this IServiceCollection services)
+        {
+            return services.ConfigureHealthCheck();
+        }
+        private static IServiceCollection ConfigureHealthCheck(this IServiceCollection services)
+        {
+            _ = services.AddHealthChecks()
+                        .AddDbContextCheck<PortalDbContext>()
+                        .AddCheck<DatabaseHealthCheck>("databaseHealthCheck");
+
+            return services;
+        }
+    }
+}
