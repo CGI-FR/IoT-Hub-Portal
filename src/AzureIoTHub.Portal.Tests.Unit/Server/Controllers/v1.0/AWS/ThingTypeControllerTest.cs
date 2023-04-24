a583311e3c8d63d6e32b132866266b4bfef9b574
GUEYE <serignesaliou.gueye@cgi.com>
2023-04-24 16:28:12 +0200

    Create Thing Type Controller tested

---
+++ src/AzureIoTHub.Portal.Tests.Unit/Server/Controllers/v1.0/AWS/ThingTypeControllerTest.cs
@@ -0,0 +1,81 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Tests.Unit.Server.Controllers.v1._0.AWS
+{
+    using System;
+    using System.Threading.Tasks;
+    using AzureIoTHub.Portal.Application.Services.AWS;
+    using AzureIoTHub.Portal.Domain.Repositories;
+    using AzureIoTHub.Portal.Server.Controllers.v1._0.AWS;
+    using AzureIoTHub.Portal.Shared.Models.v1._0.AWS;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ThingTypeControllerTest
+    {
+        private MockRepository mockRepository;
+
+        private Mock<IThingTypeService> mockThingTypeService;
+        private Mock<IThingTypeRepository> mockThingTypeRepository;
+        private Mock<IUrlHelper> mockUrlHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockThingTypeService = this.mockRepository.Create<IThingTypeService>();
+            this.mockUrlHelper = this.mockRepository.Create<IUrlHelper>();
+            this.mockThingTypeRepository = this.mockRepository.Create<IThingTypeRepository>();
+        }
+
+        private ThingTypeController CreateThingTypeController()
+        {
+            return new ThingTypeController(this.mockThingTypeService.Object)
+            {
+                Url = this.mockUrlHelper.Object
+            };
+        }
+
+        [Test]
+        public async Task CreateAThingTypeShouldReturnOK()
+        {
+            // Arrange
+            var thingTypeController = CreateThingTypeController();
+
+            var thingType = new ThingTypeDetails()
+            {
+                ThingTypeID = Guid.NewGuid().ToString()
+            };
+
+            _ = this.mockThingTypeService
+                .Setup(x => x.CreateThingType(It.Is<ThingTypeDetails>(c => c.ThingTypeID.Equals(thingType.ThingTypeID, StringComparison.Ordinal))))
+                .ReturnsAsync(thingType);
+
+            // Act
+            var result = await thingTypeController.CreateThingTypeAsync(thingType);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsAssignableFrom<OkObjectResult>(result);
+
+            var okObjectResult = result as ObjectResult;
+
+            Assert.IsNotNull(okObjectResult);
+            Assert.AreEqual(200, okObjectResult.StatusCode);
+
+            Assert.IsNotNull(okObjectResult.Value);
+            Assert.IsAssignableFrom<ThingTypeDetails>(okObjectResult.Value);
+
+            var thingTypeObject = okObjectResult.Value as ThingTypeDetails;
+            Assert.IsNotNull(thingTypeObject);
+            Assert.AreEqual(thingType.ThingTypeID, thingTypeObject.ThingTypeID);
+
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
