492b95ffa663c10406b5a9ca2579d86d12b964b0
ssgueye2 <serignesaliou.gueye@cgi.com>
2023-04-28 11:19:31 +0200

    Fix formatting

--- src/AzureIoTHub.Portal.Domain/Entities/AWS/ThingTypeTag.cs
+++ src/AzureIoTHub.Portal.Domain/Entities/AWS/ThingTypeTag.cs
@@ -7,7 +7,6 @@ namespace AzureIoTHub.Portal.Domain.Entities.AWS
 
     public class ThingTypeTag : EntityBase
     {
-        
         public string Key { get; set; } = default!;
         public string Value { get; set; } = default!;
 
