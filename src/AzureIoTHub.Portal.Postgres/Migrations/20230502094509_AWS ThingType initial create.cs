383ce5202c9237448a7fed90f114067e9fb336d9
ssgueye2 <serignesaliou.gueye@cgi.com>
2023-05-02 13:09:17 +0200

    Add Copyright CGI

--- src/AzureIoTHub.Portal.Postgres/Migrations/20230502094509_AWS ThingType initial create.cs
+++ src/AzureIoTHub.Portal.Postgres/Migrations/20230502094509_AWS ThingType initial create.cs
@@ -1,6 +1,7 @@
 
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #nullable disable
-
 namespace AzureIoTHub.Portal.Postgres.Migrations
 {
     using Microsoft.EntityFrameworkCore.Migrations;
