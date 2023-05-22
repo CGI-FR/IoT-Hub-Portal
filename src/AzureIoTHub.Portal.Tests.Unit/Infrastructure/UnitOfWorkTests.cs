34a529fd6f14d12ce8e82402598aff09cc4fcc54
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2023-05-22 11:36:56 +0200

    Revert unit tests on UnitOfWorkTests

--- src/AzureIoTHub.Portal.Tests.Unit/Infrastructure/UnitOfWorkTests.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Infrastructure/UnitOfWorkTests.cs
@@ -3,56 +3,56 @@
 
 namespace AzureIoTHub.Portal.Tests.Unit.Infrastructure
 {
+    using System.Threading;
     using System.Threading.Tasks;
-    using AutoFixture;
-    using AzureIoTHub.Portal.Domain;
-    using AzureIoTHub.Portal.Domain.Entities;
-    using AzureIoTHub.Portal.Domain.Repositories;
     using AzureIoTHub.Portal.Infrastructure;
-    using AzureIoTHub.Portal.Infrastructure.Repositories;
-    using AzureIoTHub.Portal.Tests.Unit.UnitTests.Bases;
-    using FluentAssertions;
     using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.DependencyInjection;
+    using Moq;
     using NUnit.Framework;
 
     [TestFixture]
-    public class UnitOfWorkTests : BackendUnitTest
+    public class UnitOfWorkTests
     {
-        private UnitOfWork<DbContext> unitOfWork;
+        private MockRepository mockRepository;
+        private Mock<DbContext> mockDbContext;
 
-        public override void Setup()
+        [SetUp]
+        public void SetUp()
         {
-            base.Setup();
-
-            _ = ServiceCollection.AddSingleton<IDeviceModelRepository, DeviceModelRepository>();
-            _ = ServiceCollection.AddSingleton<ILabelRepository, LabelRepository>();
-            _ = ServiceCollection.AddSingleton(DbContext);
-            _ = ServiceCollection.AddSingleton<IUnitOfWork, UnitOfWork<PortalDbContext>>();
-
-            Services = ServiceCollection.BuildServiceProvider();
-
-            this.unitOfWork = new UnitOfWork<DbContext>(DbContext);
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            this.mockDbContext = this.mockRepository.Create<DbContext>();
         }
 
         [TestCase]
         public async Task SaveAsyncTest()
         {
             // Arrange
-            var deviceModel = Fixture.Create<DeviceModel>();
-            var label = Fixture.Create<Label>();
+            using var instance = new UnitOfWork<DbContext>(this.mockDbContext.Object);
 
-            _ = await this.unitOfWork.Context.AddAsync(deviceModel);
-            _ = await this.unitOfWork.Context.AddAsync(label);
+            _ = this.mockDbContext.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(0);
+
+            _ = this.mockDbContext.Setup(c => c.Dispose());
 
             // Act
-            await this.unitOfWork.SaveAsync();
+            await instance.SaveAsync();
 
             // Assert
-            var savedDeviceModel = await this.unitOfWork.Context.Set<DeviceModel>().FindAsync(deviceModel.Id);
-            _ = savedDeviceModel.Should().BeEquivalentTo(deviceModel);
-            var savedLabel = await this.unitOfWork.Context.Set<Label>().FindAsync(label.Id);
-            _ = savedLabel.Should().BeEquivalentTo(label);
+            this.mockDbContext.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [TestCase]
+        public void DisposeTest()
+        {
+            // Arrange
+            using var instance = new UnitOfWork<DbContext>(this.mockDbContext.Object);
+            _ = this.mockDbContext.Setup(c => c.Dispose());
+
+            // Act
+            instance.Dispose();
+
+            // Assert
+            this.mockDbContext.Verify(c => c.Dispose(), Times.Once);
         }
     }
 }
