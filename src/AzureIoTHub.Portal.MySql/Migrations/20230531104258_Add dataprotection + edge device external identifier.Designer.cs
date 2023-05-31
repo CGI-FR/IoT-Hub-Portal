1b1ed46db1234bf909546d34de8f75744bbbb25f
Kevin BEAUGRAND <9513635+kbeaugrand@users.noreply.github.com>
2023-05-31 14:05:17 +0200

    Update the mysql schema to match the model (#2132)

--- src/AzureIoTHub.Portal.MySql/Migrations/20230531104258_Add dataprotection + edge device external identifier.Designer.cs
+++ src/AzureIoTHub.Portal.MySql/Migrations/20230531104258_Add dataprotection + edge device external identifier.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AzureIoTHub.Portal.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AzureIoTHub.Portal.MySql.Migrations
 {
     [DbContext(typeof(PortalDbContext))]
-    partial class PortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230531104258_Add dataprotection + edge device external identifier")]
+    partial class Adddataprotectionedgedeviceexternalidentifier
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AzureIoTHub.Portal.MySql/Migrations/20230531104258_Add dataprotection + edge device external identifier.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzureIoTHub.Portal.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adddataprotectionedgedeviceexternalidentifier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalIdentifier",
+                table: "EdgeDeviceModels",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "DataProtectionKeys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FriendlyName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Xml = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataProtectionKeys", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataProtectionKeys");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalIdentifier",
+                table: "EdgeDeviceModels");
+        }
+    }
+}
