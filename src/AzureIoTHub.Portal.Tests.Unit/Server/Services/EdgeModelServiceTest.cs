fe2d123aa61458d28f538ac8cf2208dd0b6d5cbb
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2023-06-03 22:47:47 +0200

    Fix codeql warning

--- src/AzureIoTHub.Portal.Tests.Unit/Server/Services/EdgeModelServiceTest.cs
+++ src/AzureIoTHub.Portal.Tests.Unit/Server/Services/EdgeModelServiceTest.cs
@@ -335,7 +335,7 @@ namespace AzureIoTHub.Portal.Tests.Unit.Server.Services
             var edgeDeviceModel = Fixture.Create<IoTEdgeModel>();
 
             _ = this.mockEdgeDeviceModelRepository.Setup(x => x.GetByIdAsync(It.IsAny<string>()))
-                .ReturnsAsync((EdgeDeviceModel)default);
+                .ReturnsAsync(value: null);
             _ = this.mockEdgeDeviceModelRepository.Setup(repository => repository.InsertAsync(It.IsAny<EdgeDeviceModel>()))
                 .Returns(Task.CompletedTask);
             _ = this.mockUnitOfWork.Setup(work => work.SaveAsync())
