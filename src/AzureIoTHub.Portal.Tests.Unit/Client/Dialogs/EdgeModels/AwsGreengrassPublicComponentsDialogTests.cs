696c4853f5e3aeb5ec65e30a0ce46850b11204ec
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2023-06-03 21:59:44 +0200

    Add unit tests on AwsGreengrassComponentDialog and AwsGreengrassPublicComponentsDialog

---
+++ src/AzureIoTHub.Portal.Tests.Unit/Client/Dialogs/EdgeModels/AwsGreengrassPublicComponentsDialogTests.cs
@@ -0,0 +1,161 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AzureIoTHub.Portal.Tests.Unit.Client.Dialogs.EdgeModels
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using AzureIoTHub.Portal.Client.Dialogs.EdgeModels;
+    using AzureIoTHub.Portal.Client.Services;
+    using AzureIoTHub.Portal.Models.v10;
+    using AzureIoTHub.Portal.Tests.Unit.UnitTests.Bases;
+    using Bunit;
+    using FluentAssertions;
+    using Microsoft.Extensions.DependencyInjection;
+    using Moq;
+    using MudBlazor;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AwsGreengrassPublicComponentsDialogTests : BlazorUnitTest
+    {
+        private Mock<IEdgeModelClientService> mockEdgeModelClientService;
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            this.mockEdgeModelClientService = MockRepository.Create<IEdgeModelClientService>();
+
+            _ = Services.AddSingleton(this.mockEdgeModelClientService.Object);
+        }
+
+        [Test]
+        public async Task AwsGreengrassPublicComponentsDialog_AfterOnInitializedAsync_PublicEdgeComponentsAreLoaded()
+        {
+            // Arrange
+            var edgeModules = Array.Empty<IoTEdgeModule>().ToList();
+            var publicEdgeComponents = Fixture.CreateMany<IoTEdgeModule>(10).ToList();
+
+            _ = this.mockEdgeModelClientService.Setup(s => s.GetPublicEdgeModules())
+                .ReturnsAsync(publicEdgeComponents);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"EdgeModules", edgeModules}
+            };
+
+            // Act
+            await cut.InvokeAsync(() => service?.Show<AwsGreengrassPublicComponentsDialog>(string.Empty, parameters));
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(10));
+
+            // Assert
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+
+        [Test]
+        public async Task AwsGreengrassPublicComponentsDialog_ClickOnCancel_DialogCanceled()
+        {
+            // Arrange
+            var edgeModules = Array.Empty<IoTEdgeModule>().ToList();
+            var publicEdgeComponents = Fixture.CreateMany<IoTEdgeModule>(10).ToList();
+
+            _ = this.mockEdgeModelClientService.Setup(s => s.GetPublicEdgeModules())
+                .ReturnsAsync(publicEdgeComponents);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"EdgeModules", edgeModules}
+            };
+
+            IDialogReference dialogReference = null;
+            await cut.InvokeAsync(() => dialogReference = service?.Show<AwsGreengrassPublicComponentsDialog>(string.Empty, parameters));
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(10));
+
+            // Act
+            cut.Find("#greengrass-public-components-cancel").Click();
+
+            var result = await dialogReference.Result;
+
+            // Assert
+            _ = result.Canceled.Should().BeTrue();
+            _ = edgeModules.Should().BeEmpty();
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+
+        [Test]
+        public async Task AwsGreengrassPublicComponentsDialog_ClickOnSubmitWithoutSelectingPublicComponents_EdgeModulesNotUpdated()
+        {
+            // Arrange
+            var edgeModules = Array.Empty<IoTEdgeModule>().ToList();
+            var publicEdgeComponents = Fixture.CreateMany<IoTEdgeModule>(10).ToList();
+
+            _ = this.mockEdgeModelClientService.Setup(s => s.GetPublicEdgeModules())
+                .ReturnsAsync(publicEdgeComponents);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"EdgeModules", edgeModules}
+            };
+
+            IDialogReference dialogReference = null;
+            await cut.InvokeAsync(() => dialogReference = service?.Show<AwsGreengrassPublicComponentsDialog>(string.Empty, parameters));
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(10));
+
+            // Act
+            cut.Find("#greengrass-public-components-submit").Click();
+
+            var result = await dialogReference.Result;
+
+            // Assert
+            _ = result.Canceled.Should().BeFalse();
+            _ = edgeModules.Should().BeEmpty();
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+
+        [Test]
+        public async Task AwsGreengrassPublicComponentsDialog_ClickOnSubmitAfterSelectingPublicComponent_EdgeModulesNotUpdated()
+        {
+            // Arrange
+            var edgeModules = Array.Empty<IoTEdgeModule>().ToList();
+            var publicEdgeComponents = Fixture.CreateMany<IoTEdgeModule>(10).ToList();
+
+            _ = this.mockEdgeModelClientService.Setup(s => s.GetPublicEdgeModules())
+                .ReturnsAsync(publicEdgeComponents);
+
+            var cut = RenderComponent<MudDialogProvider>();
+            var service = Services.GetService<IDialogService>() as DialogService;
+
+            var parameters = new DialogParameters
+            {
+                {"EdgeModules", edgeModules}
+            };
+
+            IDialogReference dialogReference = null;
+            await cut.InvokeAsync(() => dialogReference = service?.Show<AwsGreengrassPublicComponentsDialog>(string.Empty, parameters));
+            cut.WaitForAssertion(() => cut.FindAll("table tbody tr").Count.Should().Be(10));
+
+            // Act
+            cut.WaitForAssertion(() => cut.Find("table tbody tr").Click());
+            cut.Find("#greengrass-public-components-submit").Click();
+
+            var result = await dialogReference.Result;
+
+            // Assert
+            _ = result.Canceled.Should().BeFalse();
+            _ = edgeModules.Count.Should().Be(1);
+            cut.WaitForAssertion(() => MockRepository.VerifyAll());
+        }
+    }
+}
