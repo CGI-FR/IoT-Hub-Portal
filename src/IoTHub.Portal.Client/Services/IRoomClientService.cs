99935615aa7b975e44f04a2e226924f5b7aaf3b3
lucas-mrq <lucasmarquet63800@gmail.com>
2024-03-12 15:23:01 +0100

    Add Rooms, Planning & Schedule

---
+++ src/IoTHub.Portal.Client/Services/IRoomClientService.cs
@@ -0,0 +1,15 @@
+// Copyright (c) CGI France. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace IoTHub.Portal.Client.Services
+{
+    using System.Threading.Tasks;
+    using IoTHub.Portal.Shared.Models.v10;
+    using IoTHub.Portal.Shared.Models.v10.Filters;
+
+    public interface IRoomClientService
+    {
+        Task<string> CreateRoom(RoomDto room);
+        Task<RoomDto> GetRoom(string roomId);
+    }
+}
