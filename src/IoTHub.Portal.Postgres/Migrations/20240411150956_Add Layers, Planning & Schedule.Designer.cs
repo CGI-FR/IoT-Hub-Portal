6796afd48036ae4213742d5235a0082c1ceca401
lucas-mrq <lucasmarquet63800@gmail.com>
2024-04-11 17:14:15 +0200

    feature/add-rooms-postgree-migration-layer-planning-rooms

--- src/IoTHub.Portal.Postgres/Migrations/20240411150956_Add Layers, Planning & Schedule.Designer.cs
+++ src/IoTHub.Portal.Postgres/Migrations/20240411150956_Add Layers, Planning & Schedule.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IoTHub.Portal.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IoTHub.Portal.Postgres.Migrations
 {
     [DbContext(typeof(PortalDbContext))]
-    partial class PortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240411150956_Add Layers, Planning & Schedule")]
+    partial class AddLayersPlanningSchedule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -611,13 +614,11 @@ namespace IoTHub.Portal.Postgres.Migrations
                 {
                     b.HasOne("IoTHub.Portal.Domain.Entities.Device", null)
                         .WithMany("Tags")
-                        .HasForeignKey("DeviceId")
-                        .OnDelete(DeleteBehavior.Cascade);
+                        .HasForeignKey("DeviceId");
 
                     b.HasOne("IoTHub.Portal.Domain.Entities.EdgeDevice", null)
                         .WithMany("Tags")
-                        .HasForeignKey("EdgeDeviceId")
-                        .OnDelete(DeleteBehavior.Cascade);
+                        .HasForeignKey("EdgeDeviceId");
                 });
 
             modelBuilder.Entity("IoTHub.Portal.Domain.Entities.EdgeDevice", b =>
