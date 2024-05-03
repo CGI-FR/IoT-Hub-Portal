632a8d72aea2fb8ecc75b5bd547e9b4bd6f9d37c
Kevin BEAUGRAND <contact@kbeaugrand.fr>
2024-05-03 19:05:59 +0200

    Fix #2980 - Add cascade delete to foreign keys

--- src/IoTHub.Portal.MySql/Migrations/20240503170430_Fix #2980 - delete tag values in cascade when deleting devices.Designer.cs
+++ src/IoTHub.Portal.MySql/Migrations/20240503170430_Fix #2980 - delete tag values in cascade when deleting devices.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IoTHub.Portal.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IoTHub.Portal.MySql.Migrations
 {
     [DbContext(typeof(PortalDbContext))]
-    partial class PortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240503170430_Fix #2980 - delete tag values in cascade when deleting devices")]
+    partial class Fix2980deletetagvaluesincascadewhendeletingdevices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
