668ddeb95b0d14e31eb1d38bb12e3baf8fc51eb6
TLeoDev <115005885+TLeoDev@users.noreply.github.com>
2024-08-09 14:05:45 +0200

    merge scheduler remote repository into the new branch

--- src/IoTHub.Portal.Infrastructure/Startup/AzureServiceCollectionExtension.cs
+++ src/IoTHub.Portal.Infrastructure/Startup/AzureServiceCollectionExtension.cs
@@ -130,7 +130,7 @@ namespace IoTHub.Portal.Infrastructure.Startup
                                 var serviceClient = new BlobServiceClient(configuration.AzureStorageAccountConnectionString);
                                 var container = serviceClient.GetBlobContainerClient(opts.ImageContainerName);
 
-                                _ = container.SetAccessPolicy(PublicAccessType.Blob);
+                                //_ = container.SetAccessPolicy(PublicAccessType.Blob);
                                 _ = container.CreateIfNotExists();
 
                                 opts.BaseUri = container.Uri;
